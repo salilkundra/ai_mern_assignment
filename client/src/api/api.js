48c4e87df8649117fdbe7adef44e30d8bffa306c
MAC <136920548+salilkundra@users.noreply.github.com>
2026-03-24 17:07:36 +0530

    updated front api
    update

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api', 
+  baseURL: 'https://ai-mern-assignment.onrender.com/api', 
   headers: {
     'Content-Type': 'application/json',
   },
